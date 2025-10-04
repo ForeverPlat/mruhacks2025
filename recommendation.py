d690873a68d3bdd3dcd8ba27802b15d34064fcd0
Solakinn <nick.wa250@gmail.com>
2025-10-04 16:50:51 -0600

    rec algo finished

--- recommendation.py
+++ recommendation.py
@@ -1,4 +1,43 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
 
+df = pd.read_csv('./res/fake_restaurants_100.csv')
+
+df_drop = df.drop(columns=['id'])
+
+def get_recommendations(cost, cuisine):
+
+    for i in df_drop.index:
+        if df_drop.loc[i, "avg_cost_range"] != cost or df_drop.loc[i, "cuisine"] != cuisine:
+            df_drop.drop(i, inplace=True)
+    
+    reclist = []
+    
+    for z in range(len(df_drop['name'])):
+        row = []
+        for x in range(4):
+            if x == 1:
+                row.append(float(df_drop.iloc[z, x]))
+            else:
+                row.append(df_drop.iloc[z, x])
+        reclist.append(row)
+    return reclist
+
+def get_recommendation_from_name(name):
+     for i in df_drop.index:
+         if df_drop.loc[i, "name"] == name:
+             temp = get_recommendations(df_drop.loc[i, "avg_cost_range"], df_drop.loc[i, "cuisine"])
+             break
+     count = 0
+     for x in temp:
+         if temp[count][0] == name:
+             temp.pop(count)
+             break
+         count += 1
+     return temp
+
+# test = get_recommendations('$$', 'Caribbean')    
+# print(test)
+# test2 = get_recommendation_from_name('Oasis Tapas Bar')
+# print(test2)
+# testfloat = float(test[0][1])
+# print(testfloat)
