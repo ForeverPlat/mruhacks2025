f3679f25552898d190081722b358d02e379fb16b
foreverplat <luqman.o.ajani@gmail.com>
2025-10-05 03:33:47 -0600

    removed

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, g
 from recommendation import get_recommendations, get_recommendation_from_name
 
 app = Flask(__name__, template_folder='templates')
 
-cuisine = []
-price = ""
-restaurants = []
+# commenting this out for now, going to hard code for testing
+# restaurants = []
+# cuisine = []
+# price = ""
 
 @app.route("/")
 def home():
@@ -26,14 +27,33 @@ def form_data():
     if missing:
         return jsonify({"error": "Missing required fields.", "missing": sorted(missing)}), 400
 
+    restaurants = data["restaurants"]  # e.g., list or string—your choice
     cuisine = data["cuisines"]
     price = data["price"]
-    restaurants = data["restaurants"]  # e.g., list or string—your choice
+
+    with open("res/preference.txt", "w") as f:
+        if (len(restaurants) == 0):
+            f.write("\n")
+
+        for i in range(len(restaurants)):
+            if (i == len(restaurants) - 1):
+                f.write(restaurants[i]+"\n")
+            else:
+                f.write(restaurants[i]+ ",")
+
+        f.write(price + "\n")
+
+        for i in range(len(cuisine)):
+            if (i == len(cuisine) - 1):
+                f.write(cuisine[i]+"\n")
+            else:
+                f.write(cuisine[i]+ ",")
+        
 
     # Do something with the data
-    print("restaurants:", restaurants)
-    print("cuisines:", cuisine)
-    print("price:", price)
+    # print("restaurants:", restaurants)
+    # print("cuisines:", cuisine)
+    # print("price:", price)
 
     return jsonify({
         "ok": True,
@@ -47,16 +67,30 @@ def show_recommendations():
 
 @app.route("/api/recommendation", methods=["GET"])
 def get_recommendations():
-    data = request.get_json(silent=True)
+
+    restaurants = []
+    price = ""
+    cuisines = []
+
+    with open("res/preference.txt", "r") as preferenceFile:
+        for index, line in enumerate(preferenceFile):
+            if index == 0:
+                restaurants = line.strip().split(',')
+            elif index == 1:
+                price = line.strip()
+            elif index == 2:
+                cuisines = line.strip().split(',')
+
+    print(restaurants)
+    print(price)
+    print(cuisines)
     
-    if not data:
-        return jsonify({"error": "No JSON body provided or invalid JSON."}), 400
-    
-    if (len(cuisine) == 0 and price != "" and len(restaurants) == 0):
+    if (len(cuisines) == 0 and price != "" and restaurants[0] == ''):
         return jsonify("error: Missing preferences."), 400
     
-    if (len(restaurants) == 0):
-        recommendations = get_recommendations()
+    if (restaurants[0] == ''):
+        recommendations = get_recommendations(price, cuisines)
+        print("after get rec")
         return jsonify({
             "ok": True,
             "data": recommendations
@@ -65,8 +99,18 @@ def get_recommendations():
         # return the data
     
         
-    if (len(restaurants) != 0):
-        recommendations = get_recommendation_from_name()
+    if (restaurants[0] != ''):
+        recommendations = []
+        recommendations = get_recommendation_from_name(restaurants)
+        # for restaurant in restaurants:
+        #     recommendation = get_recommendation_from_name(restaurant)
+        #     recommendations.append(recommendation)
+
+        print("rec")
+        print(recommendations)
+        # for r in recommendations:
+            # print(r)
+
         return jsonify({
             "ok": True,
             "data": recommendations
